c149de13db41d0359dffe46d65d0d332e32fd7d3
mohamedali477 <mohamedali4880@gmail.com>
2021-10-05 06:10:06 +0200

    Fixes

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ if(process.env.NODE_ENV === 'production')
     })
 }
 
-const port = process.env.PORT || 3000 ;
+const port = process.env.PORT || 8000 ;
 
 app.listen(port, ()=> console.log('node server started') );
 
