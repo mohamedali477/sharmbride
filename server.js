ead6c6ed948e3e606950e9cb78254c7da554dfbf
mohamedali477 <mohamedali4880@gmail.com>
2021-10-05 05:59:49 +0200

    Fix

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ if(process.env.NODE_ENV === 'production')
     app.use('/',express.static('/client/build'))
     app.get('*',(req,res)=>{
 
-        res.sendFile(path.resolve(__dirname,'client/build/index.html'))
+        res.sendFile(path.resolve(__dirname,'/client/build/index.html'))
     })
 }
 
-const port = process.env.PORT || 5000 ;
+const port = process.env.PORT || 3000 ;
 
 app.listen(port, ()=> console.log('node server started') );
 
